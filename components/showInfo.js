4995591e16c84f1eb8e9d4af7a2be71e746b024a
Micaela Analia Jofre <mkjofre@gmail.com>
2022-06-23 23:04:23 -0300

    button select

--- components/showInfo.js
+++ components/showInfo.js
@@ -16,15 +16,26 @@ AFRAME.registerComponent('show-info', {
         let textPos = -1.7;
         let textH = 2.6;
         var color = this.color;
-
+        var infoPanel = this.infoPanel = document.querySelector('#infoPanel')
+        
         //evento boton del menu cava
-        var cavaButton = this.cavaButton = document.querySelectorAll('.cavaButton')
-            
+        var cavaButtons =document.querySelectorAll('.cavaButton')
+        var buttons = this.buttons = [];
+        
+        for (let i = 0; i < cavaButtons.length; i++) {
+            const el = cavaButtons[i];
+            var button = {
+                el: el,
+                color: el.getAttribute("color")
+            }
+            buttons.push(button);
+        }
+
         this.onMouseEnter = this.onMouseEnter.bind(this);
         this.onMouseLeave = this.onMouseLeave.bind(this);
-        for (let i = 0; i < cavaButton.length; ++i) {
-            cavaButton[i].addEventListener('mouseenter', this.onMouseEnter);
-            cavaButton[i].addEventListener('mouseleave', this.onMouseLeave);
+        for (let i = 0; i < cavaButtons.length; ++i) {
+            cavaButtons[i].addEventListener('mouseenter', this.onMouseEnter);
+            cavaButtons[i].addEventListener('mouseleave', this.onMouseLeave);
         }
 
         
@@ -32,35 +43,34 @@ AFRAME.registerComponent('show-info', {
         let info = [
             {
                 title: 'Cocina',
-                img: '../img/organizadores.jpg',
+                img: './img/organizadores.jpg',
                 text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s rd dummy text ever since the 1500 s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but als.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500.',
             },
             {
                 title: 'Cabernet',
-                img: '../img/cabernet.jpg',
+                img: './img/cabernet.jpg',
                 text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s rd dummy text ever since the 1500 s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but als.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s rd dummy text ever since the 1500 s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but als.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500.',
             },
             {
                 title: 'Malbec',
-                img: '../img/malbec.jpg',
+                img: './img/malbec.jpg',
                 text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s rd dummy text ever since the 1500 s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but als.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500.',
             },
             {
                 title: 'Pinot',
-                img: '../img/pinot.jpg',
+                img: './img/pinot.jpg',
                 text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s rd dummy text ever since the 1500 s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but als.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500.',
             },
             {
                 title: 'Tipos de reservas',
-                img: '../img/wino.jpg',
+                img: './img/wino.jpg',
                 text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s rd dummy text ever since the 1500 s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but als.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry standard dummy text ever since the 1500.',
             }
         ]
         
-        el.addEventListener('click', function () {
+        el.addEventListener('click', function (evt) {
             //panel que se despliega con el icono info
-            let infoPanel = document.querySelector('#infoPanel')
-
+            
             //info del cartel 
             let title = document.querySelector('#title')
             let img = document.querySelector('#img')
@@ -68,6 +78,21 @@ AFRAME.registerComponent('show-info', {
             let container = document.querySelector('#infoPanel')
             let currentTextH = textH;
 
+            for (let u = 0; u < buttons.length; u++) {
+                const element = buttons[u].el;
+                if(evt.target === element)
+                    if(evt.target.is("clicked"))
+                        evt.target.setAttribute('material', 'color', button.color);
+                    else {
+                        evt.target.setAttribute('material', 'color', 'red')
+                        evt.target.addState("clicked")
+                    }
+                        
+                else if(evt.target.is("clicked"))
+                    evt.target.removeState("clicked")
+            }
+
+            
             
             //ubicacion panel y contenido
             if (el.id === 'cabernet') {
@@ -119,6 +144,7 @@ AFRAME.registerComponent('show-info', {
                 img.setAttribute("height", imgH*.6)
                 text.setAttribute('value', info[0].text)
                 currentTextH = 2;
+
             }
 
             //setear posicion y tamaño
@@ -141,46 +167,64 @@ AFRAME.registerComponent('show-info', {
             //cambio de boton actual con click
             if (infoPanel.getAttribute('current') === el.id || infoPanel.getAttribute('current') === 'null') {
                 if (infoPanel.getAttribute('visible')) {
+
                     infoPanel.setAttribute('visible', 'false')
                     infoPanel.setAttribute('current', 'null')
+                    for (let i = 0; i < buttons.length; i++) {
+                        const button = buttons[i];
+                        button.el.setAttribute("material", "color", button.color)
+                        
+                    }
 
                 } else {
                     infoPanel.setAttribute('visible', 'true')
                     infoPanel.setAttribute('current', el.id)
                 }
             } else {
+                
+                for (let i = 0; i < buttons.length; i++) {
+                    const button = buttons[i];
+                    if(button.el.id == infoPanel.getAttribute('current')){
+                        let currentButton = document.querySelector("#" + infoPanel.getAttribute('current'))
+                        currentButton.setAttribute("material", "color", button.color)
+                    }
+                }
                 infoPanel.setAttribute('current', el.id)
             }
             
         })
+
     },
     onMouseEnter: function (evt) {
             
-        let button = this.cavaButton
+        let infoPanel = this.infoPanel
+        let buttons = this.buttons
         
-        for (let i = 0; i < button.length; ++i) {
-            
-            if (evt.target === button[i]) {
-                this.color = evt.target.getAttribute('material').color
-                console.log(this.color);
-                evt.target.setAttribute('material', 'color', '#046de7');
-                evt.target.object3D.scale.set(1.2, 1.2, 1.2);
-                this.el.addState('hover');
+        let current = infoPanel.getAttribute("current")
+        for (let i = 0; i < buttons.length; ++i) {
+            let button = buttons[i]
+            if (evt.target === button.el) {
+                if(current !== evt.target.id) {
+                    evt.target.setAttribute('material', 'color', '#046de7');
+                    evt.target.object3D.scale.set(1.2, 1.2, 1.2);
                 }
+            }
         }
         
-        },
+    },
 
     onMouseLeave: function (evt) {
-        let button = this.cavaButton
-        if (this.el.is('hover')) {
-            console.log(this.color);
-            evt.target.object3D.scale.set(1, 1, 1);
-            evt.target.setAttribute('material', 'color', this.color);
-        }
-        this.el.removeState('hover');
-            
+        let infoPanel = this.infoPanel
+        let buttons = this.buttons
+        let current = infoPanel.getAttribute("current")
         
-        },
-
+        for (let i = 0; i < buttons.length; ++i) {
+            let button = buttons[i]
+            if (evt.target === button.el) {
+                evt.target.object3D.scale.set(1, 1, 1);
+                if(current !== evt.target.id)
+                    evt.target.setAttribute('material', 'color', button.color);
+            }
+        }
+    }
 });
